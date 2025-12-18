d6c91fb1ff63488e01e47f8cf66f025794833d02
Jackson Santos <devjackson.contato@gmail.com>
2025-12-17 21:17:01 -0300

    feat(backend): add cors

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI
 from backend.controllers.tickets_controller import router as tickets_router
 from backend.infra.seeds.seed_tickets import run_seeds
 from backend.controllers.metrics_controller import router as metrics_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",  # apenas front local
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(tickets_router, prefix="/tickets")
 app.include_router(metrics_router, prefix="/metrics")
